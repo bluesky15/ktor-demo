ba3a277844a28dde25fa66bc4be46829e797f71e
lalitbehera <lalitkumar.b@landmarkgroup.in>
2020-02-10 14:49:34 +0530

    html dsl style html creation

--- src/Application.kt
+++ src/Application.kt
@@ -1,22 +1,29 @@
 package com.lkb.ktordemo
 
-import io.ktor.application.*
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.html.respondHtml
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import kotlinx.html.*
 
-fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, port = 8080) {
-        routing {
-            get("/") {
-                call.respondText("Hello World!", ContentType.Text.Plain)
-            }
-            get("/demo") {
-                call.respondText("HELLO WORLD!")
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/"){
+            call.respondHtml {
+                head {
+                    title { +"Async World" }
+                }
+                body {
+                    h1 {
+                        +"Title"
+                    }
+                }
             }
         }
     }
-    server.start(wait = true)
 }
